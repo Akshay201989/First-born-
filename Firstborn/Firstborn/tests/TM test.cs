1a133343bc56417feb8beabc6b9ef7f34e556af8
Akshay201989 <akshay201989@gmail.com>
2023-02-25 11:54:59 +1300

    nunit implemented

---
+++ Firstborn/Firstborn/tests/TM test.cs
@@ -0,0 +1,26 @@
+﻿using Firstborn.pages;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using Firstborn.utilities;
+
+        IWebDriver chrome = new ChromeDriver();
+
+        
+        //login page object intialization and definition
+
+        loginpage loginpageobj = new loginpage();
+        loginpageobj.LogInActions(chrome);
+
+        homegape HomePageObj = new homegape();
+        HomePageObj.GoToTMPage(chrome);
+    
+        tmpage TmPageOBJ = new tmpage();
+        TmPageOBJ.CreateTM(chrome);
+
+        TmPageOBJ.EditTM(chrome);
+
+        TmPageOBJ.DeleteTM(chrome);
+
+        chrome.Quit();
+          
+   
